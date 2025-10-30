4a74096053727665601384b683108c90044c115c
Aldo Silvestre <aldosilvestre89@gmail.com>
2025-10-30 02:20:52 -0300

    Added new layer GAMING initial

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -20,10 +20,11 @@
 #define CAPSLOCK_INDICATOR_BRIGHTNESS_INC 76
 
 enum layer_namKC {
-    _BASE,
+    _BASE = 0,
     _LOWER,
     _RAISE,
-    _ADJUST
+    _ADJUST,
+    _GAMING
 };
 
 enum {
@@ -76,13 +77,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 //`----------------------------------'  `------------------------------------'
 	),
    [_ADJUST] =	LAYOUT(
-          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PSCR,  KC_APP,  KC_INS,   KC_NO,   KC_NO, KC_BSPC,
+          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PSCR,  KC_APP,  KC_INS,   KC_NO,   KC_NO,   KC_NO,
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RM_TOGG, RM_HUEU, RM_SATU, RM_SPDU, RM_VALU,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        RM_TOGG, RM_HUEU, RM_SATU, RM_SPDU, RM_VALU,   KC_NO,                      KC_MRWD, KC_MPLY, KC_MFFD,   KC_NO,   KC_NO,   KC_NO,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RM_NEXT, RM_HUED, RM_SATD, RM_SPDD, RM_VALD,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        RM_NEXT, RM_HUED, RM_SATD, RM_SPDD, RM_VALD,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG(4),
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                         _______,   _______, KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, _______, KC_LALT, _______
+                                 //`---------------------------------'  `-----------------------------------'
+	),
+   [_GAMING] =	LAYOUT(
+        KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,   KC_0,  KC_MINS,
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+         KC_TAB,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,                      MS_BTN1, MS_BTN2, MS_BTN3,   KC_NO,   KC_NO,   KC_NO,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_LCTL,   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG(4),
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                          _______,   _______, KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, _______, KC_LALT, _______
                                  //`---------------------------------'  `-----------------------------------'
@@ -92,16 +105,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP) },
-    [_LOWER] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
-    [_RAISE] = { ENCODER_CCW_CW(KC_MS_WH_LEFT, KC_MS_WH_RIGHT), ENCODER_CCW_CW(_______, _______) },
-    [_ADJUST] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
+    [_BASE]   = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_LOWER]  = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [_RAISE]  = { ENCODER_CCW_CW(MS_WHLL, MS_WHLR), ENCODER_CCW_CW(_______, _______) },
+    [_ADJUST] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [_GAMING] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
 };
 #endif
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-		if (host_keyboard_led_state().caps_lock) {
-			rgb_matrix_set_color_all(0,255,0);
+    if (host_keyboard_led_state().caps_lock) {
+		  rgb_matrix_set_color_all(0,255,0);
+      return false;
+		}
+
+    switch (biton32(layer_state)) {
+			case _GAMING:
+				  rgb_matrix_set_color_all(255,0,0);
+				break;
 		}
 	return false;
 }
